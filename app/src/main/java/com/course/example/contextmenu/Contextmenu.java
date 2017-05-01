7899b273a0573b1e13eaf309570ddda76e1e1270
Jim Pepe <jim.pepe@verizon.net>
2017-05-01 16:46:16 -0400

    Initial commit

---
+++ app/src/main/java/com/course/example/contextmenu/Contextmenu.java
@@ -0,0 +1,52 @@
+package com.course.example.contextmenu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class Contextmenu extends Activity {
+
+	private ImageView ImView;
+	
+	final int PICK1 = Menu.FIRST + 1;
+	final int PICK2 = Menu.FIRST + 2;
+	final int PICK3 = Menu.FIRST + 3;
+	final int PICK4 = Menu.FIRST + 4;
+		
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        ImView = (ImageView)findViewById(R.id.image);
+        ImView.setImageResource(R.drawable.trek);
+        
+        registerForContextMenu(ImView);
+    }
+    
+    @Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {		
+		super.onCreateContextMenu(menu, v, menuInfo);
+		//add a couple of options to the context menu
+		menu.setHeaderTitle("Select Special Action");
+		menu.add(0, PICK1, Menu.NONE, "Option-1 ONE ");
+		menu.add(0, PICK2, Menu.NONE, "Option-2 TWO ");
+    }
+
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		super.onContextItemSelected(item);
+		String title = item.getTitle().toString(); //get menu item title
+		int itemId = item.getItemId();             //get menu item id
+		Toast.makeText(this, title + " " + itemId, Toast.LENGTH_LONG).show();
+		return false;
+	}
+}
